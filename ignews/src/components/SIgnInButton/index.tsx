3a2f9ee502cab194724bffaa8304d30aeff23141
Pedro Moreno <nennogabriel@gmail.com>
2021-04-13 14:38:03 -0300

    next: 02 05-06

--- ignews/src/components/SIgnInButton/index.tsx
+++ ignews/src/components/SIgnInButton/index.tsx
@@ -2,17 +2,27 @@ import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 import styles from './styles.module.scss';
 
-export function SignInButton() {
-  const isUserLoggedIn = true;
+import { signIn, signOut, useSession } from 'next-auth/client';
 
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+export function SignInButton() {
+  const [session] = useSession();
+
+  return session ? (
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04b361" />
-      Diego Fernandes
+      {session.user.name}
       <FiX className={styles.closeIcon} color="#737370" />
     </button>
   ) : (
-    <button className={styles.signInButton} type="button">
+    <button
+      className={styles.signInButton}
+      type="button"
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
